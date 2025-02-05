6ea3e2107b3a94ed38b41983a8c6b13cdbbcc916
Neha <neha.22b0151015@abes.ac.in>
2025-02-05 09:58:31 +0530

    changes

--- CE/file1.js
+++ CE/file1.js
@@ -1,7 +1,15 @@
 const fs= require('node:fs');
-// const data=fs.readFileSync("./data.txt","utf-8");
+const data=fs.readFileSync("./data.txt","utf-8");
+console.log(data);
 // console.log(data.toString());
 
-fs.writeFileSync("./data1.txt","CE ABES promise");
-fs.appendFileSync("./data1.txt","AKTU");
-fs.renameSync("./data1.txt","./data2.txt");
+// fs.writeFileSync("./data1.txt","CE ABES promise");         //write something on file
+// fs.appendFileSync("./data1.txt","AKTU");                  //overwrite
+// fs.renameSync("./data1.txt","./data2.txt");            //chg the file name
+// fs.unlinkSync("./data2.txt");                    //delete file
+
+if(data.match("H")){
+    console.log("H is present");
+    const changedata=data.replace("H","ABES");
+    fs.writeFileSync("./data.txt",changedata);
+}
