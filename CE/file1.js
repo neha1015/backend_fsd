5d2b2b5aa0876de47e97fe90313573b856df3c87
Neha <neha.22b0151015@abes.ac.in>
2025-02-05 09:46:12 +0530

    adding files

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,7 @@
 const fs= require('node:fs');
-const data=fs.readFileSync("./data.txt","utf-8");
-console.log(data.toString());
+// const data=fs.readFileSync("./data.txt","utf-8");
+// console.log(data.toString());
+
+fs.writeFileSync("./data1.txt","CE ABES promise");
+fs.appendFileSync("./data1.txt","AKTU");
+fs.renameSync("./data1.txt","./data2.txt");
